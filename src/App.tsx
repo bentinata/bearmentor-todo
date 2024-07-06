b0cf1b5eaec127b88fb09ca16c09853abef92ba2
Ben Nata <_@bentinata.com>
2024-07-06 16:09:29 +0700

    feat: Add and remove task

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,46 @@
 import { useState } from "react";
-import "./App.css"
+import "./App.css";
 import { Task, TaskList } from "./Task";
 
 function App() {
-  const [tasks, setTasks] = useState<Task[]>([{
-    title: "Task 0",
-    status: "COMPLETED"
-  }]);
+  const [tasks, setTasks] = useState<Task[]>([
+    {
+      title: "Task 0",
+      status: "COMPLETED",
+    },
+  ]);
 
   function addTask() {
     const newTask: Task = {
       title: `Task ${tasks.length + 1}`,
-      status: "CREATED"
-    }
-    setTasks((prevTasks) => [...prevTasks, newTask])
+      status: "CREATED",
+    };
+    setTasks((prevTasks) => [...prevTasks, newTask]);
   }
 
-  return (<>
-    <button onClick={addTask}>Add task</button>
-    <TaskList tasks={tasks} />
-  </>
+  function deleteTask(deletedTitle: string) {
+    setTasks((prevTasks) =>
+      prevTasks.filter((task) => task.title !== deletedTitle),
+    );
+  }
+
+  function completeTask(completedTitle: string) {
+    setTasks((prevTasks) =>
+      prevTasks.map((task) =>
+        task.title === completedTitle ? { ...task, status: "COMPLETED" } : task,
+      ),
+    );
+  }
+
+  return (
+    <>
+      <button onClick={addTask}>Add task</button>
+      <TaskList
+        tasks={tasks}
+        onDelete={deleteTask}
+        onCompleted={completeTask}
+      />
+    </>
   );
 }
 
--- src/Task.tsx
+++ src/Task.tsx
@@ -1,16 +1,44 @@
 export interface Task {
   title: string;
-  status: 'CREATED' | 'COMPLETED'
+  status: "CREATED" | "COMPLETED";
 }
 
 interface TaskOverviewProps {
-  task: Task
+  task: Task;
+  onDelete: Function;
+  onCompleted: Function;
 }
 
-export function TaskOverview({ task }: TaskOverviewProps) {
-  return (<div>{task.title}{task.status}</div>)
+export function TaskOverview({
+  task,
+  onDelete,
+  onCompleted,
+}: TaskOverviewProps) {
+  return (
+    <div className="container mx-auto border p-4 m-5 rounded-2xl shadow-lg">
+      <h1 className="text-base">{task.title}</h1>
+      <span className="text-sm">{task.status}</span>
+      <button onClick={() => onDelete(task.title)}>Delete</button>
+      <button onClick={() => onCompleted(task.title)}>Complete</button>
+    </div>
+  );
 }
 
-export function TaskList({ tasks}: { tasks: Task[]}) {
-  return tasks.map(task => <TaskOverview task={task} />)
+export function TaskList({
+  tasks,
+  onDelete,
+  onCompleted,
+}: {
+  tasks: Task[];
+  onDelete: Function;
+  onCompleted: Function;
+}) {
+  return tasks.map((task, id) => (
+    <TaskOverview
+      key={id}
+      task={task}
+      onDelete={onDelete}
+      onCompleted={onCompleted}
+    />
+  ));
 }
